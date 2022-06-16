822d5375fa43990aafef7273a1191b1e76fa139a
mxw74 <marcus.walls@utexas.edu>
2022-06-16 17:45:29 -0500

    make check for win function work

--- scripts.js
+++ scripts.js
@@ -12,7 +12,6 @@ let board = [
   ["", "", ""] // <-- Row 3, index 2
 ]
 
-
 // this "handleClick" function is called when a box is clicked. Here, "element" will hold the same value as "this" does in the HTML. 
 // "this" is a special word in JS but "element" could have been "thing" or "el" or whatever we wanted it to be as long as we use it again in the "console.log" statement
 const handleClick = (element) => {
@@ -23,12 +22,14 @@ const handleClick = (element) => {
   // this next line prevents an X being changed to an O or an O being changed to an X by...
   //  checking to see if the square clicked has anything in it, if not continue
   if(!document.getElementById(element.id).innerHTML){
+    const row = parseInt(element.id.charAt(0)) //moved lines 25-27 above the addmarker
+    const column = parseInt(element.id.charAt(2))
+    board[row][column] = currentMarker //wanted to define what the behind the scenes board has in each box in each row and column 
+    console.log("board", board)
     addMarker(element.id)
   }
 
-  const row = parseInt(element.id.charAt(0))
-  const column = parseInt(element.id.charAt(2))
-  board[row][column] = currentMarker
+
 
 }
 
@@ -55,72 +56,16 @@ const addMarker = (id) => {
   // .innerHTML 
 
   document.getElementById(id).innerHTML=currentMarker
-  checkForWin()
+  checkForWin() //threw it in here so that after it adds the marker it can go to my check for win function
 }
 
-
-
-
-
-
-
-
-
-
-
-
-// This "changeMarker" function changes "X" to "O" in the "currentMarker" variable or "O" to "X"
-const changeMarker = () => {
-  if(currentMarker === "X"){
-    currentMarker = "O"
-  } else {
-    currentMarker = "X"
-  }
-}
-
-
-
-
-
-
-
-
-
-
-
-
-// This "resetBoard" function is called when the user clicks on the "Restart" button.
-const resetBoard = () => {
-  
-  // @TODO-3: To make your "Restart" button work you'll need to build a line of code here that:
-      // collects all of the "td" elements into an HTML Collection: https://www.w3schools.com/jsref/dom_obj_htmlcollection.asp  
-    
-  // @TODO-3.5: MIX & MATCH, You will need the following pieces of code to build that line:
-  // squares
-  // .getElementsByTagName("TD")
-  // =
-  // document
-  // const
-  const squares = document.getElementsByTagName("TD")
-  // loops over the HTML Collection of TDs and clears out the Xs and Os
-  for (i=0; i < squares.length; i++) {
-
-    // will log out the id of each square as it loops over them.
-    console.log(squares[i].id)
-
-    // sets the innerHTML to null to replace the "X" or "O"
-    squares[i].innerHTML = null
-  }  
-}
-
-const checkForWin = () => {
+const checkForWin = () => { 
   if(horizontalWin() || verticalWin() || diagonalWin()) {
     window.alert(`Player ${currentMarker} won!`)
   } else {
     changeMarker()
   }
 }
-
 
 // trying to make code work
 
@@ -168,4 +113,45 @@ else if((board[0][2] == "X" && board[1][1] == "X" && board[2][0] == "X")
 else {return false}
 }
 
-// checkForWin()
+
+// This "changeMarker" function changes "X" to "O" in the "currentMarker" variable or "O" to "X"
+const changeMarker = () => {
+  if(currentMarker === "X"){
+    currentMarker = "O"
+  } else {
+    currentMarker = "X"
+  }
+}
+
+
+// This "resetBoard" function is called when the user clicks on the "Restart" button.
+const resetBoard = () => {
+  
+  // @TODO-3: To make your "Restart" button work you'll need to build a line of code here that:
+      // collects all of the "td" elements into an HTML Collection: https://www.w3schools.com/jsref/dom_obj_htmlcollection.asp  
+    
+  // @TODO-3.5: MIX & MATCH, You will need the following pieces of code to build that line:
+  // squares
+  // .getElementsByTagName("TD")
+  // =
+  // document
+  // const
+  const squares = document.getElementsByTagName("td")
+  // loops over the HTML Collection of TDs and clears out the Xs and Os
+  for (i=0; i < squares.length; i++) {
+
+    // will log out the id of each square as it loops over them.
+    console.log(squares[i].id)
+
+    // sets the innerHTML to null to replace the "X" or "O"
+    squares[i].innerHTML = null
+
+  }  
+
+  board = [ // put this in to make my behind the scenes board clear when i reset, but if I took this back out and tried then the console would still show the Xs and Os in it
+    ["", "", ""], // <-- Row 1, index 0
+    ["", "", ""], // <-- Row 2, index 1
+    ["", "", ""] // <-- Row 3, index 2
+  ]
+  
+}
